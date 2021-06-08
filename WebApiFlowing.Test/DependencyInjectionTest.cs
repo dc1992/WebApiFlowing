7df4e540a7ff110f642086be4075523a9f10e6d8
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-06-08 23:13:17 +0200

    [Test]

---
+++ WebApiFlowing.Test/DependencyInjectionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
+using WebApiFlowing.Data.Interfaces;
+
+namespace WebApiFlowing.Test
+{
+    [TestFixture]
+    public class DependencyInjectionTest
+    {
+        private IHost _host;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _host = Program
+                .CreateHostBuilder(null)
+                .Build();
+
+            Environment.SetEnvironmentVariable("CONNECTION_STRING", "testConnection");
+        }
+
+        [Test]
+        public void Startup_ShouldHaveIDataContextRegistered()
+        {
+            Assert.IsNotNull(_host.Services.GetRequiredService<IDataContext>());
+        }
+
+        [Test]
+        public void Startup_ShouldHaveIUserRepositoryRegistered()
+        {
+            Assert.IsNotNull(_host.Services.GetRequiredService<IUserRepository>());
+        }
+    }
+}
