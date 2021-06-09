10308b36f8973641848fa944454392d5c97d312f
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-06-09 21:38:22 +0200

    refactor

--- WebApiFlowing/Controllers/UserWeightsController.cs
+++ WebApiFlowing/Controllers/UserWeightsController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebApiFlowing.BusinessLogic.Extensions;
 using WebApiFlowing.Data.Interfaces;
+using WebApiFlowing.DTOs;
 using WebApiFlowing.DTOs.Response;
+using WeightHistory = WebApiFlowing.DTOs.Response.WeightHistory;
 
 namespace WebApiFlowing.Controllers
 {
@@ -25,6 +27,13 @@ namespace WebApiFlowing.Controllers
             var user = await _userRepository.GetUserInfosBy(userGuid);
             user.ShouldNotBeNull();
 
+            var response = GetUserWeightsResponseFrom(user);
+
+            return response;
+        }
+
+        private UserWeightsResponse GetUserWeightsResponseFrom(User user)
+        {
             var response = new UserWeightsResponse
             {
                 WeightHistories = user.WeightHistories
