f5eaca3029193b83181c18df67515a68d4a0371a
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-06-09 17:31:25 +0200

    implemented controller logic

--- WebApiFlowing/Controllers/UserWeightsController.cs
+++ WebApiFlowing/Controllers/UserWeightsController.cs
@@ -22,8 +22,18 @@ namespace WebApiFlowing.Controllers
         [HttpGet]
         public async Task<UserWeightsResponse> Get(Guid userGuid)
         {
+            var user = await _userRepository.GetUserInfosBy(userGuid);
+            user.ShouldNotBeNull();
+
             var response = new UserWeightsResponse
             {
+                WeightHistories = user.WeightHistories
+                    .OrderBy(wh => wh.DateOfMeasurement)
+                    .Select(wh => new WeightHistory
+                    {
+                        DateOfMeasurement = wh.DateOfMeasurement,
+                        WeightInKgs = wh.WeightInKgs
+                    }).ToList()
             };
 
             return response;
