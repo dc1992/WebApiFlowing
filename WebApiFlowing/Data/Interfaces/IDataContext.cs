ffe2a2e3ea742eaf34701f861c6f1eea0bc12f9b
Diego Ceccacci <diegoceccacci92@gmail.com>
2021-06-12 18:56:27 +0200

    implemented insert user repository

--- WebApiFlowing/Data/Interfaces/IDataContext.cs
+++ WebApiFlowing/Data/Interfaces/IDataContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using WebApiFlowing.Data.Models;
 
 namespace WebApiFlowing.Data.Interfaces
@@ -6,5 +8,7 @@ namespace WebApiFlowing.Data.Interfaces
     public interface IDataContext
     {
         DbSet<User> Users { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
